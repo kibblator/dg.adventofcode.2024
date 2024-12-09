57bc5a9c87728a12aef2586ed793d5f0cde71ea6
kibblator <kornfreakuk@hotmail.com>
2024-12-09 22:39:25 +0000

    chore: solve parts 1 & 2 of day 9

---
+++ __tests__/day9.test.ts
@@ -0,0 +1,52 @@
+import { getEntireTextFile, getGridFromFile } from "../src/utils/inputReader";
+import { defrag } from "../src/day9/index";
+
+describe("Day 9", () => {
+  test("Part 1 Sample", () => {
+    const input = getEntireTextFile("./src/day9/sample_input.txt");
+
+    const result = defrag(input);
+
+    expect(result).toEqual(1928);
+  });
+
+  test("Part 1 Sample 2", () => {
+    const input = getEntireTextFile("./src/day9/sample_input_2.txt");
+
+    const result = defrag(input);
+
+    console.log(`Sample 2 looks like ${result}`);
+  });
+
+  test("Part 1 Test Input", () => {
+    const input = getEntireTextFile("./src/day9/test_input.txt");
+
+    const result = defrag(input);
+
+    console.log(`Day 9 Part 1 is: ${result}`);
+  });
+
+  test("Part 2 Sample", () => {
+    const input = getEntireTextFile("./src/day9/sample_input.txt");
+
+    const result = defrag(input, true);
+
+    expect(result).toEqual(2858);
+  });
+
+  test("Part 2 Sample", () => {
+    const input = getEntireTextFile("./src/day9/sample_input_2.txt");
+
+    const result = defrag(input, true);
+
+    expect(result).toEqual(2858);
+  });
+
+  test.only("Part 2 Test Input", () => {
+    const input = getEntireTextFile("./src/day9/test_input.txt");
+
+    const result = defrag(input, true);
+
+    console.log(`Day 9 Part 2 is: ${result}`);
+  });
+});
---
+++ src/day9/index.ts
@@ -0,0 +1,128 @@
+const getCurrentFileSystem = (fileSystem: string, part2enabled: boolean) => {
+  let systemString = [];
+  let index = 0;
+
+  for (
+    var currentBlock = 0;
+    currentBlock <= fileSystem.length;
+    currentBlock += 2
+  ) {
+    const fileBlockSize = +fileSystem[currentBlock];
+    const freeSpace = +fileSystem[currentBlock + 1];
+
+    for (let i = 0; i < fileBlockSize; i++) {
+      systemString.push(index.toString());
+    }
+
+    if (freeSpace) {
+      systemString.push(...Array(freeSpace + 1).join("."));
+    }
+
+    index++;
+  }
+
+  return systemString;
+};
+
+const endsInAllDots = (array: string[]) => {
+  const numDots = array.filter((cfs) => cfs === ".").length;
+  const lastNChars = array.slice(-numDots);
+  return lastNChars.every((char) => char === ".");
+};
+
+const rearrangeFileSystem = (currentFileSystemArray: string[]) => {
+  for (let i = currentFileSystemArray.length - 1; i > 0; i--) {
+    if (currentFileSystemArray[i] !== ".") {
+      const index = currentFileSystemArray.indexOf(".");
+      const temp = currentFileSystemArray[i];
+
+      currentFileSystemArray[i] = currentFileSystemArray[index];
+      currentFileSystemArray[index] = temp;
+    }
+    if (endsInAllDots(currentFileSystemArray)) {
+      console.log("Ends in all dots");
+      return currentFileSystemArray;
+    }
+  }
+
+  return currentFileSystemArray;
+};
+
+const rearrangeFileSystem2 = (currentFileSystemArray: string[]) => {
+  for (let i = currentFileSystemArray.length - 1; i > 0; i--) {
+    if (currentFileSystemArray[i] !== ".") {
+      const temp = currentFileSystemArray[i];
+
+      let fileMarkerIndex = currentFileSystemArray.indexOf(temp);
+      i = fileMarkerIndex;
+      let occurrences = 0;
+      while (temp === currentFileSystemArray[fileMarkerIndex]) {
+        occurrences++;
+        fileMarkerIndex++;
+      }
+
+      const index = currentFileSystemArray.findIndex((_, idx, arr) => {
+        return (
+          idx <= arr.length - occurrences &&
+          arr.slice(idx, idx + occurrences).every((dot) => dot === ".")
+        );
+      });
+
+      if (index >= i) {
+        continue;
+      }
+
+      let dotMarker = index;
+      let dotOccurences = 0;
+      while (currentFileSystemArray[dotMarker] === ".") {
+        dotOccurences++;
+        dotMarker++;
+      }
+
+      if (occurrences <= dotOccurences) {
+        for (var x = 0; x < occurrences; x++) {
+          currentFileSystemArray[index + x] = temp;
+        }
+        for (var y = 0; y < occurrences; y++) {
+          currentFileSystemArray[i + y] = ".";
+        }
+      }
+    }
+  }
+
+  return currentFileSystemArray;
+};
+
+const getChecksum = (fileSystem: string[]) => {
+  let result = 0;
+
+  for (let i = 0; i < fileSystem.length; i++) {
+    if (fileSystem[i] === ".") {
+      continue;
+    }
+
+    const value = i * +fileSystem[i];
+    result += value;
+  }
+
+  return result;
+};
+
+export const defrag = (input: string, part2enabled: boolean = false) => {
+  const fileSystem = getCurrentFileSystem(input, part2enabled);
+
+  console.log(`Current file system is ${fileSystem}`);
+
+  let rearrangedFileSystem;
+  if (part2enabled) {
+    rearrangedFileSystem = rearrangeFileSystem2(fileSystem);
+  } else {
+    rearrangedFileSystem = rearrangeFileSystem(fileSystem);
+  }
+
+  console.log(`Rearranged file system is ${rearrangedFileSystem}`);
+
+  const checkSum = getChecksum(rearrangedFileSystem);
+
+  return checkSum;
+};
