b27dc2c65c13410063a8b940a0a7560f22c0c43e
kibblator <kornfreakuk@hotmail.com>
2024-12-27 22:25:51 +0000

    chore: added caching for part 2

--- src/day11/index.ts
+++ src/day11/index.ts
@@ -2,35 +2,41 @@ let numBlinks = 0;
 
 const blink = (
   stone: number,
-  stoneCount: number = 1,
-  currentBlink: number = 0
+  currentBlink: number = 0,
+  cache: any = {}
 ): number => {
   if (currentBlink === numBlinks) {
-    return stoneCount;
+    return 1;
   }
 
+  const cacheKey = `${stone}|${currentBlink}`;
+
   currentBlink++;
+
+  if (cache[cacheKey]) {
+    return cache[cacheKey];
+  }
+
   if (stone === 0) {
-    return blink(1, stoneCount, currentBlink);
+    cache[cacheKey] = blink(1, currentBlink, cache);
   } else if (stone.toString().length % 2 === 0) {
     const middle = Math.floor(stone.toString().length / 2);
     const start = +stone.toString().substring(0, middle);
     const end = +stone.toString().substring(middle);
 
-    return (
-      blink(start, stoneCount, currentBlink) +
-      blink(end, stoneCount, currentBlink)
-    );
+    cache[cacheKey] =
+      blink(start, currentBlink, cache) + blink(end, currentBlink, cache);
   } else {
-    return blink(stone * 2024, stoneCount, currentBlink);
+    cache[cacheKey] = blink(stone * 2024, currentBlink, cache);
   }
+
+  return cache[cacheKey];
 };
 
 export const pebbles = (input: string, part2: boolean = false) => {
   let stones = input.split(" ").map((s) => +s);
 
   numBlinks = part2 ? 75 : 25;
-
   let numStones = 0;
 
   for (var i = 0; i < stones.length; i++) {
